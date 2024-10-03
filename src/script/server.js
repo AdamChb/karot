defb19b0ab3000295edc53c185352b86988a167f
Sacha Portal <sacha.portal@efrei.net>
2024-10-03 16:49:09 -0400

    Added comments for server explanation + mysql2 package

--- src/script/server.js
+++ src/script/server.js
@@ -1,14 +1,25 @@
+// Require is the keyword used to import modules
+// You can import local files by using the relative path
+
 const express = require("express");
 const api_db = require("../database/api_db");
 const init_db = require("../database/init_db");
+
+// Configuration about the server
 const hostname = "127.0.0.1";
 const port = 3000;
 
+// Create a new instance of express
 const server = express();
+
+// When a GET requets is made at the adress /getMostLiked, the server respond (res) with the return of the function getMostLiked
 
 server.get("/getMostLiked", async (req, res) => {
   res.send(await api_db.getMostLiked(10));
 });
+
+// The same method exist with POST, PUT and DELETE
+// Request body is in req.body, it contains all the data sent by the client in the request body
 
 server.get("/initdb", async (req, res) => {
   await init_db.doAll();
