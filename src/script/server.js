388a88613083ae0f4d461bb46e9202b6d29d6bf7
Sacha Portal <sacha.portal@efrei.net>
2024-10-07 23:44:37 -0400

    Update of database files

--- src/script/server.js
+++ src/script/server.js
@@ -4,7 +4,6 @@
 const express = require("express");
 const api_db = require("../database/api_db");
 const init_db = require("../database/init_db");
-const db = require("../database/start_db");
 
 // Configuration about the server
 const hostname = "127.0.0.1";
@@ -29,6 +28,5 @@ server.get("/initdb", async (req, res) => {
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
-  db.start_connect();
   console.log(`Server running at http://${hostname}:${port}/`);
 });
